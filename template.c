995daced3e7e0b27e994012fd16ca98b63ad9b22
PrinceBot-Ratthanin <50706633+PrinceBot-Ratthanin@users.noreply.github.com>
2019-08-11 10:41:16 +0700

    Add files via upload

--- template.c
+++ template.c
@@ -1,4 +1,14 @@
 #include "NKP_ONE.h"
+float Kp = 0.5 ;
+  float Ki = 0;
+  float Kd = 0;
+  uint16_t setpoint;
+  float present_position;
+  float errors = 0;
+  float output = 0;
+  float integral ;
+  float derivative ;
+  float previous_error;
 
 
 ${EXTINC}
