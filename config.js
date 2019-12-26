d09ce1661c803defb42c4a4b4b0b6f3464e03b66
PrinceBot-Ratthanin <50706633+PrinceBot-Ratthanin@users.noreply.github.com>
2019-12-26 23:25:51 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {    
+    name : 'NKP_ONE',
+    platform : 'arduino-esp32',
+    title : 'NKP_ONE',
+    description : 'บอร์ดสำหรับการเรียนรู้การควบคุมหุ่นยนต์เบื้องต้น',
+    author : 'PrinceBot',
+    website : 'http://www.princebot.net/',
+    email : 'print081@gmail.com',
+    git : 'https://github.com/PrinceBot-Ratthanin/NKP_ONE-KB-IDE/',    
+    image : '/static/NKP_ONE.jpg',
+    version : '1.1.2'
+};
